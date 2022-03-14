a426206f963de49bfab13f3d022b8d5238509365
Johoseong <whghtjd320@naver.com>
2022-03-15 01:31:27 +0900

    #19 refactoring: refactoring DI of SecurityConfig

--- src/main/java/com/example/algoproject/security/SecurityConfig.java
+++ src/main/java/com/example/algoproject/security/SecurityConfig.java
@@ -15,7 +15,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final JWTUtil jwtUtil;
+    private final JWTAuthenticationFilter jwtAuthenticationFilter;
 
     @Override
     public void configure(WebSecurity web) {
@@ -34,6 +34,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/api/user/login").permitAll()
                 .and()
-                .addFilterBefore(new JWTAuthenticationFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class);
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
     }
 }
