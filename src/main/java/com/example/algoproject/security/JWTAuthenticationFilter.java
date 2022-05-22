e3e845ad1e5ad6f1b2dfcdbb16f3a68beca35302
Johoseong <whghtjd320@naver.com>
2022-05-22 18:34:42 +0900

    #52 refactoring: refactoring custom error handler

--- src/main/java/com/example/algoproject/security/JWTAuthenticationFilter.java
+++ src/main/java/com/example/algoproject/security/JWTAuthenticationFilter.java
@@ -15,6 +15,8 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @RequiredArgsConstructor
