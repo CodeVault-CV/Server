cd886fdf366a94138952aa09be4fee4358bcc837
Johoseong <whghtjd320@naver.com>
2022-08-17 16:48:14 +0900

    #127 refactoring: add language in solution detail response

--- src/main/java/com/example/algoproject/solution/dto/response/SolutionInfo.java
+++ src/main/java/com/example/algoproject/solution/dto/response/SolutionInfo.java
@@ -1,6 +1,7 @@
 package com.example.algoproject.solution.dto.response;
 
 import com.example.algoproject.review.domain.Review;
+import com.example.algoproject.solution.domain.Language;
 import lombok.Data;
 
 import javax.persistence.Lob;
@@ -19,6 +20,8 @@ public class SolutionInfo {
     @Lob
     private String readMe;
 
+    private Language language;
+
     @NotBlank
     private Timestamp date; //등록 날짜/시간
 
@@ -26,9 +29,10 @@ public class SolutionInfo {
 
     private String name;
 
-    public SolutionInfo(String code, String readMe, Timestamp date, String id, String name) {
+    public SolutionInfo(String code, String readMe, Language language, Timestamp date, String id, String name) {
         this.code = code;
         this.readMe = readMe;
+        this.language = language;
         this.date = date;
         this.id = id;
         this.name = name;
